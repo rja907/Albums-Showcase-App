0e92fc99f9eb3adfdd6401d830782beeafb42673
Raj <kmrrajj@gmial.com>
2017-12-15 00:05:09 +0530

    final app commit

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const {
     headerContentStyle,
     headerTextStyle,
@@ -30,7 +30,7 @@ const AlbumDetail = ({ album }) => {
       </CardSection>
 
       <CardSection>
-        <Button />
+        <Button onPress={() => Linking.openURL(url)}>Buy Now</Button>
       </CardSection>
     </Card>
   );
--- albums/src/components/Button.js
+++ albums/src/components/Button.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = () => {
+const Button = ({ onPress, children }) => {
   const { buttonStyle, textStyle } = styles;
   return (
-    <TouchableOpacity
-      onPress={() => console.log('pressed')}
-      style={buttonStyle}
-    >
-      <Text style={textStyle}>Click me!</Text>
+    <TouchableOpacity onPress={onPress} style={buttonStyle}>
+      <Text style={textStyle}>{children}</Text>
     </TouchableOpacity>
   );
 };
